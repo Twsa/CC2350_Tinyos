a95445496c835354d507ae3ca9593137be6c9077
Twsa <liuchuanjiacl@gmail.com>
2017-06-03 17:07:14 +0800

    tinyos-cc2530 仓库已创建

---
+++ tinyos.sh
@@ -0,0 +1,9 @@
+#Hello
+export TOSROOT=/opt/tinyos-2.x
+export TOSDIR=$TOSROOT/tos
+export CLASSPATH='cygpath -w $TOSROOT\tinyos-2.x\support\sdk\java\tinyos.jar'
+export CLASSPATH=$CLASSPATH
+export MAKERULES=$TOSROOT/support/make/Makerules
+#"/opt/tinyos-2.x/support/make/Makerules"
+#export -p
+echo 'Done.'
